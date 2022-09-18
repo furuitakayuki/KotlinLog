4c7a7ff1f460da48f29a9ca7f076475a0b6934b0
furuitakayuki <maki1904@icloud.com>
2022-09-19 01:24:37 +0900

    Humanクラス

--- app/src/main/java/jp/techacademy/takayuk/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/takayuk/kotlinlog/Human.kt
@@ -2,16 +2,9 @@ package jp.techacademy.takayuk.kotlinlog
 
 import  android.util.Log
 
-import android.util.Log
-
 open class Human:Animal {
     // 引数付きコンストラクタ
-    constructor(name: String,age: Int): super(name,age) {
-    }
-
-    // Animalクラスのメソッドをオーバーライド
-    override fun say() {
-        Log.d("kotlintest","私の名前は" + this.name + "です。年は" + this.age + "歳です。")
+    constructor(name: String, age: Int): super(name,age) {
     }
 
     // Animalクラスのメソッドをオーバーライド
