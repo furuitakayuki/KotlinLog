f0fa93cc44ab8c09d0c0a13767ba4c24daf3343f
furuitakayuki <maki1904@icloud.com>
2022-09-18 22:29:22 +0900

    １０から５０に変える

--- app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
@@ -2,13 +2,23 @@ package jp.techacademy.takayuk.kotlinlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import  android.util.Log    //ここを追加
+import  android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("kotlintest","ログへの出力テスト")    //ここを追加
+        Log.d("kotlintest","ログへの出力テスト")
+
+        // 整数型の変数をnumという名前で作成して、１０を代入する
+        var num = 10
+        Log.d("kotlintest",Integer.toString(num))
+
+        // numに５０を代入する
+        // 変数や定数に .tostring() と記述する方法もある
+        num = 50
+        Log.d("kotlintest",num.toString())
+
     }
 }
