6b22d07ce4d8919e1ace42eec04f9c8c9e1b470b
furuitakayuki <maki1904@icloud.com>
2022-09-18 22:35:14 +0900

    if文

--- app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
@@ -33,6 +33,20 @@ class MainActivity : AppCompatActivity() {
         val num3 = 20
         Log.d("kotlintest","num2 < num3 = " + (num2 < num3))
 
+        //if文
+        num = 60
+
+        if (num >=90) {
+            Log.d("kotlintest","優")
+        } else if (num >= 75){
+            Log.d("kotlintest","良")
+        } else if (num >= 60){
+            Log.d("kotlintest","可")
+        } else {
+            Log.d("kotlintest","不可")
+        }
+
+
 
     }
 }
