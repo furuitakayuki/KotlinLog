cea0e12c463e58ef492dee133b2696bbf8466072
furuitakayuki <maki1904@icloud.com>
2022-09-18 22:32:16 +0900

    演算子

--- app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/kotlinlog/MainActivity.kt
@@ -20,5 +20,19 @@ class MainActivity : AppCompatActivity() {
         num = 50
         Log.d("kotlintest",num.toString())
 
+        //演算子
+        val num1 = 10+5-2*4/2
+        Log.d("kotlintest","10 + 5 - 2 * 4 / 2 = " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest","flag1 && flag2 = " + (flag1 && flag2))
+        Log.d("kotlintest","flag1 || flag2 = " + (flag1 || flag2))
+
+        val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest","num2 < num3 = " + (num2 < num3))
+
+
     }
 }
