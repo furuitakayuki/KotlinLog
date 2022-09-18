8c617cb2b0b80c7a87a6ebd9fdb29e9224f90a61
furuitakayuki <maki1904@icloud.com>
2022-09-19 01:05:15 +0900

    Humanクラス

--- app/src/main/java/jp/techacademy/takayuk/kotlinlog/Animal.kt
+++ app/src/main/java/jp/techacademy/takayuk/kotlinlog/Animal.kt
@@ -1,15 +1,9 @@
 package jp.techacademy.takayuk.kotlinlog
 
-abstract class Animal {
-    // プロパティ
-    var name: String
-    var age: Int
-
-    // 引数付きコンストラクタ
-    constructor(name: String,age: Int) {
-        this.name = name
-        this.age = age
-    }
+abstract class Animal// 引数付きコンストラクタ
+    (// プロパティ
+    var name: String, var age: Int
+) {
 
     abstract fun say()
 }
